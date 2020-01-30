67d8436e2c282495e6450fb748a75c9e5d8a9d51
Sarvesh <sarveshkhemlani@gmail.com>
2020-01-29 23:55:27 -0500

    Added project template

---
+++ generate.py
@@ -0,0 +1,13 @@
+# TODO: OAuth 
+#   https://github.com/reddit-archive/reddit/wiki/API
+#   https://github.com/reddit-archive/reddit/wiki/OAuth2
+#   https://www.reddit.com/prefs/apps
+#   https://www.youtube.com/watch?v=CPbvxxslDTU
+# TODO: Save auth details in config file
+#   Import config file
+
+def main():
+    print('Feels good to be writing Python again')
+
+if __name__ == "__main__":
+    main()
